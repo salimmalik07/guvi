2272e8986a7c5885dec38a1c92de738486ca7ffd
salimmalik07 <42864470+salimmalik07@users.noreply.github.com>
2019-02-07 13:26:42 +0430

    Update greatest.py

--- greatest.py
+++ greatest.py
@@ -1,8 +1,9 @@
-a=int(input())
-b=int(input())
-y=int(input())
-if (a>b) and (a>y):
-	print(a)
+a,b,y=raw_input().split()
+a=int(a)
+b=int(b)
+y=int(y)
+if (a>y) and (a>b):
+    print(a)
 elif (b>y) and (b>a):
 	print(b)
 else:

87d268848c2db3a6114dc53a97cafc687e108f86
salimmalik07 <42864470+salimmalik07@users.noreply.github.com>
2019-02-07 13:52:30 +0430

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,9 +1,11 @@
-m=int(input())
-if(m%400==0):
-    print ("leapyear")
-elif(m%4==0):
-    print ("leapyear")
-elif(m%100!=0):
-    print ("Yes")
+while True:
+	try:
+		f1=int(input())
+		break
+	except:
+		print('invalid')
+		break
+if f1%400==0 or f1%4==0 and f1%100!=0:
+	print('yes')
 else:
-    print ("No")
+	print('no')

17d5ad4b5c71b33849a58e19386d5d2ef0b741aa
salimmalik07 <42864470+salimmalik07@users.noreply.github.com>
2019-02-08 09:07:48 +0430

    Create palindrom.py

---
+++ palindrom.py
@@ -0,0 +1,15 @@
+def main():
+ m=int(input(""))
+ temp=m
+ rev=0
+ while(m>0):
+    dig=m%10
+    rev=rev*10+dig
+    m=m//10
+ if(temp==rev):
+    print("Yes")
+ else:
+    print("no")
+
+if __name__ == '__main__':
+    main()

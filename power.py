8c63ae89c045bb0134c6badbcbbd180e7bc8f220
salimmalik07 <42864470+salimmalik07@users.noreply.github.com>
2019-02-08 08:58:43 +0430

    Update power.py

--- power.py
+++ power.py
@@ -1,9 +1,14 @@
-m,n=raw_input().split()
-m=int(m)
-n=int(n)
-i=0
-ac=0
-while i < m:
-    ac+=n
-    i+=1
-print(ac)
+while True:
+	try:
+		m, b= raw_input().split( )
+		m=int(m)
+		b=int(b)
+		break
+	except:
+		print("Invalid input")
+		break
+c=1
+for x in range(b):
+	c=c*m
+print(c)
+	

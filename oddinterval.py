3df699fec4e99bc69db0b8a779ead816dcdf9108
salimmalik07 <42864470+salimmalik07@users.noreply.github.com>
2019-02-08 10:54:24 +0430

    Update oddinterval.py

--- oddinterval.py
+++ oddinterval.py
@@ -1,7 +1,7 @@
-t1,t2=raw_input().split()
+t1,t4=raw_input().split()
 t1=int(t1,)
-t2=int(t2,)
-for i in range(t1+1,t2+1):
+t4=int(t4,)
+for i in range(t1+1,t4+1):
 	if(i%2!=0):
 		i=int(i)
 		print i,

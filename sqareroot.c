717034eaf3f3c0d6d2cd4e5bd44f2da3c0ae35e3
salimmalik07 <42864470+salimmalik07@users.noreply.github.com>
2019-08-01 21:13:44 +0530

    Create sqareroot.c

---
+++ sqareroot.c
@@ -0,0 +1,34 @@
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    int a,b,c,det;
+    float r1,r2,imaginary,real;
+
+    scanf("%d%d%d",&a,&b,&c);
+    det=b*b-4*a*c;
+    if(det>0)
+    {
+       r1=(-b+sqrt(det))/(2*a);
+       r1=(-b-sqrt(det))/(2*a);
+       printf("%f",r1);
+    printf("%f",r2);
+    }
+    else if(det==0)
+    {
+       r1=r2=-b/(2*a);
+        printf("%f",r1);
+    }
+    else 
+    {
+      real = -b/(2*a);
+       imaginary = sqrt(-det) / (2*a);
+       printf("root1 = %.2f + %.2fi  root2 = %.2f - %.2fi", real, imaginary, real, imaginary);
+    }            
+
+    
+    return 0;
+}
